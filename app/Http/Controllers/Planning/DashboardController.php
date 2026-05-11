7b9262c09e42614cff0ba5c4ce37a63a4b128458
Taskspeed <taskspeed2002@gmail.com>
2026-05-11 13:20:28 +0800

    modified: functions

--- app/Http/Controllers/Planning/DashboardController.php
+++ app/Http/Controllers/Planning/DashboardController.php
@@ -5,29 +5,31 @@ namespace App\Http\Controllers\Planning;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OfficeOpcrPendingResource;
 use App\Services\DashboardService;
+use App\Traits\ApiResponseTrait;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     //
+    use ApiResponseTrait;
 
     protected $dashboardService;
 
 
     public function __construct(DashboardService $dashboardService)
     {
-         return $this->dashboardService = $dashboardService;
+        return $this->dashboardService = $dashboardService;
     }
 
 
     // number of opcr status
-    public function numberOfStatus($semester,$year){
+    public function numberOfStatus($semester, $year)
+    {
 
-    $result  =  $this->dashboardService->status($semester, $year);
+        $result  =  $this->dashboardService->status($semester, $year);
 
-    return $result;
-
+        return $result;
     }
 
     //list of the opcr pending
@@ -38,15 +40,9 @@ class DashboardController extends Controller
         $result  =  $this->dashboardService->opcrPending($semester, $year);
 
         if ($result->isEmpty()) {
-            return response()->json([
-                'message' => 'There is no data available yet.'
-            ], 200); // use 200,
+            return  $this->infoMessage('No records found', 200);
         }
 
         return OfficeOpcrPendingResource::collection($result);
     }
-
-
-
-
 }
