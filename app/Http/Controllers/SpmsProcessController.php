24b61d5798635f3f810dd7250f6ef9c1e9c5c89d
Taskspeed <taskspeed2002@gmail.com>
2026-06-18 14:18:05 +0800

    added: controller SpmsProcessController

---
+++ app/Http/Controllers/SpmsProcessController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Spms\UpdateIpcrRequest;
+use App\Http\Requests\Spms\UpdateOpcrRequest;
+use App\Http\Requests\Spms\UpdateUnitWorkPlanRequest;
+use App\Services\UpdateSpmsService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SpmsProcessController extends Controller
+{
+    //
+
+    protected UpdateSpmsService $updateService;
+
+    public function __construct(UpdateSpmsService $updateService)
+    {
+      $this->updateService = $updateService;
+    }
+
+    // update unit work plan
+    public function updateUnitWorkPlan(UpdateUnitWorkPlanRequest $request){
+
+        $authUser = Auth::user();
+
+        $validatedData = $request->validated();
+        $data = $this->updateService->unitWorkPlan($validatedData, $authUser);
+
+        return $data;
+
+    }
+
+    // update opcr
+    public function updateOpcr(UpdateOpcrRequest $request){
+
+        $authUser = Auth::user();
+
+        $validatedData = $request->validated();
+        $data = $this->updateService->opcr($validatedData, $authUser);
+
+        return $data;
+
+    }
+
+    // update ipcr
+    public function updateIpcr(UpdateIpcrRequest $request){
+
+        $authUser = Auth::user();
+
+        $validatedData = $request->validated();
+        $data = $this->updateService->ipcr($validatedData, $authUser);
+
+        return $data;
+
+    }
+}
---
+++ app/Services/UpdateSpmsService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\OfficeOpcrRecord;
+use App\Models\TargetPeriodRecord;
+use App\Models\UnitWorkPlanRecord;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class UpdateSpmsService
+{
+    // update unit work plan
+    public function unitWorkPlan(?array $validatedData, Authenticatable $authUser)
+    {
+        $records = [];
+
+        foreach ($validatedData['unitworkplan_id'] as $id) {
+            $records[] = UnitWorkPlanRecord::create([
+                'unitworkplan_id'   => $id,
+                'date'              => now()->toDateString(),
+                'status'            => $validatedData['status'],
+                'remarks'           => $validatedData['remarks'] ?? null,
+                'processed_by_name' => $authUser->name,
+                'processed_by'      => $authUser->id,
+            ]);
+        }
+        return $records;
+    }
+
+    // update opcr
+    public function opcr(?array $validatedData, Authenticatable $authUser)
+    {
+       $records = [];
+
+        foreach ($validatedData['office_opcr_id'] as $id) {
+            $records[] = OfficeOpcrRecord::create([
+                'office_opcr_id'   => $id,
+                'date'              => now()->toDateString(),
+                'status'            => $validatedData['status'],
+                'remarks'           => $validatedData['remarks'] ?? null,
+                'processed_by_name' => $authUser->name,
+                'processed_by'      => $authUser->id,
+            ]);
+        }
+
+        return $records;
+    }
+
+    // update ipcr
+    public function ipcr(?array $validatedData, Authenticatable $authUser)
+    {
+        $records = [];
+
+        foreach ($validatedData['target_period_id'] as $id) {
+            $records[] = TargetPeriodRecord::create([
+                'target_period_id'   => $id,
+                'date'              => now()->toDateString(),
+                'status'            => $validatedData['status'],
+                'remarks'           => $validatedData['remarks'] ?? null,
+                'processed_by_name' => $authUser->name,
+                'processed_by'      => $authUser->id,
+            ]);
+        }
+
+        return $records;
+    }
+}
