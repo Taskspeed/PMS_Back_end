9c6c85728d534e32129d5b3b46585ea2bd0e59f7
Taskspeed <taskspeed2002@gmail.com>
2026-07-03 15:23:29 +0800

    add: model position ranks

---
+++ app/Models/PositionRank.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PositionRank extends Model
+{
+    //
+
+    protected $table = 'position_ranks';
+
+    protected $fillable = [
+        'position_name'
+    ];
+}
